ad8135db38a98322e52bf8045cf472619dc869f7
EndzeitBegins <16666115+EndzeitBegins@users.noreply.github.com>
2021-12-01 19:49:52 +0100

    solves day 1 of Advent of Code 2021

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,25 @@
 fun main() {
+    fun List<Int>.countIncreasingMeasurements() = this
+        .zipWithNext { previousDepth, currentDepth -> currentDepth > previousDepth }
+        .count { it }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::toInt)
+            .countIncreasingMeasurements()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input
+            .map(String::toInt)
+            .windowed(3) { it.sum() }
+            .countIncreasingMeasurements()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
     println(part1(input))
